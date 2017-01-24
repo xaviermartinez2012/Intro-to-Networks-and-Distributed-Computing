2419053735c471fb8895a19f9768b54b659d6898
xaviermartinez2012 <xaviermartinez2012@gmail.com>
2017-01-24 13:38:56 -0800

    Creating Repo for CECS 327

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CECS 327 Projects
+//
+//  Created by Xavier Martinez on 1/24/17.
+//  Copyright © 2017 Xavier Martinez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
